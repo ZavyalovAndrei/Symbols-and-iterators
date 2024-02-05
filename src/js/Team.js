a5005eb2ffc9ad7d54d0393bbdd565a538cff0f3
ZavyalovAndrei <zaver1983@google.com>
2024-02-05 21:39:16 +0100

    Update Team.js and Team.test.js

--- src/js/Team.js
+++ src/js/Team.js
@@ -1,19 +1,6 @@
 export default class Team {
   constructor(currentTeam) {
     this.currentTeam = currentTeam;
-    this.index = 0;  
+    this.iterator = this.currentTeam[Symbol.iterator]();
   }
-      next() {
-        if (this.index >= this.currentTeam.length) {
-          return {
-            value: undefined,
-            done: true,
-          };
-        }
-        this.index++;
-        return {
-          value: this.currentTeam[this.index - 1],
-          done: false,
-        };
-    }
 }
--- src/js/__tests__/Team.test.js
+++ src/js/__tests__/Team.test.js
@@ -4,26 +4,28 @@ import { Undead } from "../Undead";
 import { Bowerman } from "../Bowerman";
 import Team from "../Team";
 
-const team = new Team(
-  [new Magician("Юлия"), new Daemon("Марина"),
+const team = new Team([
+  new Magician("Юлия"),
+  new Daemon("Марина"),
   new Undead("Вероника"),
-  new Bowerman("Светлана")]
-);
+  new Bowerman("Светлана"),
+]);
 
 test.each([
-  ["Magician Юлия (уровень: 1, здоровье: 100, атака: 10, защита: 40)", false],
-  ["Daemon Марина (уровень: 1, здоровье: 100, атака: 10, защита: 40)", false],
-  ["Undead Вероника (уровень: 1, здоровье: 100, атака: 25, защита: 25)", false],
-  ["Bowerman Светлана (уровень: 1, здоровье: 100, атака: 25, защита: 25)", false],
-])("should return caracter %s and done to be %s" , (expected, status) => {
-  const result = team.next();
+  ["Magician Юлия (уровень: 1, здоровье: 100, атака: 10, защита: 40)"],
+  ["Daemon Марина (уровень: 1, здоровье: 100, атака: 10, защита: 40)"],
+  ["Undead Вероника (уровень: 1, здоровье: 100, атака: 25, защита: 25)"],
+  [
+    "Bowerman Светлана (уровень: 1, здоровье: 100, атака: 25, защита: 25)"
+  ],
+])("should return caracter %s and done to be %s", (expected) => {
+  const result = team.iterator.next();
   expect(result.value.toString()).toEqual(expected);
-  expect(result.done).toBe(status);
+  expect(result.done).toBe(false);
 });
 
-test('should return undefined when done is true', () => {
-    const result = team.next();
-    console.log(result.value + "  " + result.done);
-    expect(result.value).toEqual(undefined);
-    expect(result.done).toBe(true);
-  });
+test("should return undefined when done is true", () => {
+  const result = team.iterator.next();
+  expect(result.value).toEqual(undefined);
+  expect(result.done).toBe(true);
+});
